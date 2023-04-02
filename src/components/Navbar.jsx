5ba372ad136b9e5537c1b3ad6a137473526b55c2
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-02 10:42:26 -0400

    Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="container-nav">
+                <img src="./img/logo.png" alt="Logo" className="logo" />
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">Study</a>
+                    <a href="#">Works</a>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
